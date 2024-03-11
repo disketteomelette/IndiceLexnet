7a880309d6f8bf7d2e8ae23743915a724d6968ad
disketteomelette <39524746+disketteomelette@users.noreply.github.com>
2024-03-11 19:03:39 +0100

    Update indice-lexnet.bat
    añado mensaje, limpio tras instalación (o intento de instalación) de dependencias, limpieza, etc.

--- indice-lexnet.bat
+++ indice-lexnet.bat
@@ -1,13 +1,13 @@
 @echo off
-
 REM Instalar dependencias de Python
 pip install PyQt5
 pip install reportlab
 pip3 install PyQt5
 pip3 install reportlab
-
+clear
 REM Ejecutar el script "indice.py"
-python3 indice.py
-
-pause
-
+echo "################################################################################"
+echo "# Se está ejecutando IndiceLexnet v.2 - NO CIERRE ESTA VENTANA MIENTRAS LO USA #"
+echo "################################################################################"
+echo.
+python3 indice.py 
