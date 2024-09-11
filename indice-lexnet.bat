d44adbaf5b7060e213d8ea426101c798338b9e4b
disketteomelette <39524746+disketteomelette@users.noreply.github.com>
2024-09-11 10:12:55 +0200

    Update indice-lexnet.bat
    Corrección de errores en la instalación de dependencias, eliminación de referencias a python en versiones distintas a la 3.

--- indice-lexnet.bat
+++ indice-lexnet.bat
@@ -1,11 +1,6 @@
 @echo off
-REM Instalar dependencias de Python
-pip install PyQt5
-pip install reportlab
-pip3 install PyQt5
-pip3 install reportlab
-clear
-REM Ejecutar el script "indice.py"
+echo --- Comprobando dependencias...
+python3 -m pip install PyQt5 reportlab 
 echo "################################################################################"
 echo "# Se está ejecutando IndiceLexnet v.2 - NO CIERRE ESTA VENTANA MIENTRAS LO USA #"
 echo "################################################################################"
