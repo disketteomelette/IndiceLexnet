96f0bfc496bf082fd415051e761a05db8c18123f
disketteomelette <39524746+disketteomelette@users.noreply.github.com>
2024-09-11 10:17:54 +0200

    Update indice-lexnet.bat

--- indice-lexnet.bat
+++ indice-lexnet.bat
@@ -2,7 +2,7 @@
 echo --- Comprobando dependencias...
 python3 -m pip install PyQt5 reportlab 
 echo "################################################################################"
-echo "# Se está ejecutando IndiceLexnet v.2 - NO CIERRE ESTA VENTANA MIENTRAS LO USA #"
+echo "# Se esta ejecutando IndiceLexnet v.2 - NO CIERRE ESTA VENTANA MIENTRAS LO USA #"
 echo "################################################################################"
 echo.
 python3 indice.py 
